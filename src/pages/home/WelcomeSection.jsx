7d377aa2e3c7989679ad609dfaf754d87c57e916
Carlos Dradá <carlos.drada@correounivalle.edu.co>
2025-05-14 22:17:54 -0500

    intentando arreglar el canva para que se vea centrado desde jsx

--- src/pages/home/WelcomeSection.jsx
+++ src/pages/home/WelcomeSection.jsx
@@ -3,10 +3,9 @@ import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
 import "./WelcomeSection.css";
 import { Link } from "react-router-dom";
-import Staging from "../home/staging/Staging";
 
 // Modelo 3D cargado y rotando
-function LiverCirrhosisModel() {
+function TiredPersonModel() {
   const { scene } = useGLTF("/models/liver-cancer.glb");
   const modelRef = useRef();
 
@@ -20,8 +19,6 @@ function LiverCirrhosisModel() {
   return <primitive ref={modelRef} object={scene} scale={25} position={[0, 0, 0]} />;
 }
 
-
-
 export default function WelcomeSection() {
   return (
     <div className="welcome-container">
@@ -30,13 +27,12 @@ export default function WelcomeSection() {
  <p>Explora el bienestar de tu higado de nuevas maneras. Nuestra aplicación te guía hacia un recorrido por tu higado.<p>
  </p>Diviertete aprendiendo y no permitas que tu higado se destruya como el que veras a continuacion.</p>
  
-      <div className="model-viewer">
-        <Canvas camera={{ position: [0, 0, 5] }} style={{ width: "100%", height: "100%" }}>
-          <Staging/>
+      <div className="model-viewer" >
+        <Canvas camera={{ position: [0, 0, 5] }}>
           <ambientLight intensity={0.5} />
           <directionalLight position={[0, 5, 5]} />
           <Suspense fallback={null}>
-            <LiverCirrhosisModel />
+            <TiredPersonModel />
             <OrbitControls enableZoom={false} />
           </Suspense>
         </Canvas>
@@ -44,7 +40,7 @@ export default function WelcomeSection() {
       <p>A continuacion, veras algunas enfermedades que pueden afectar a tu higado.<p>
       </p>Pero no te preocupes, con el cuidado y tratamiento necesario tendras un higado sano y feliz</p>
       <br></br>
-      <Link to="/enfermedades" className="nav-link"><button>Explorar</button></Link>
-    </div>
-  );
+      <Link to="/enfermedades" className="nav-link"><button onClick={() => alert("¡Vamos allá!")}>Explorar</button></Link>
+    </div>
+  );
 }
