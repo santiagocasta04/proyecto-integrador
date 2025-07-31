dfa495894bea3ddae6d83608c8639538066ac215
Daniel <danielarte149@gmail.com>
2025-07-30 21:42:38 -0500

    Juntando ambos cambios v2

--- src/pages/diseases/EHA.jsx
+++ src/pages/diseases/EHA.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense, useRef, useState } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
 import { OrbitControls, useGLTF } from "@react-three/drei";
-import "./EHA.css";
+import "./fattyLiver.css";
 import { Link } from "react-router-dom";
 
 
